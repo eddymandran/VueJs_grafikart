8a7cb5eeb507db9412ccf586705eb263efc34f2c
eddymandran <eddy.mandran@gmail.com>
2021-01-20 09:01:28 +0100

    moitié du chapitre

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ new Vue({
     el:'#app',
     data: {
         message: 'Salut les gens',
-        link: 'www.google.fr'
+        link: 'http://google.fr',
+        success: true,
+        persons: ['Jonathan', 'Marion', 'Manon', 'Jean']
     }
 })
